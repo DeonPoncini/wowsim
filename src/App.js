6da3a4551f1ca7eb0cf067433b3c2e521fe268ff
Deon Poncini <deonponcini@gmail.com>
2020-02-14 09:49:18 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Button, Modal, Table } from 'react-bootstrap';
 import './App.css';
+import {Belts, Boots} from './gear.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Picker extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props.gear);
+        this.state = {
+            show: false,
+        };
+    }
+
+    setShow = (s) => {
+        this.setState({show: s});
+    }
+
+    handleShow = () => {
+        this.setShow(true);
+    }
+
+    handleClose = () => {
+        this.setShow(false);
+    }
+
+    render() {
+        const rows = []
+        for (var i = 0; i < this.props.gear.length; i++) {
+            rows.push(<li key={i}>{this.props.gear[i].name}</li>);
+        }
+        return(
+            <div className="Picker">
+                <Button variant="primary" onClick={this.handleShow}>
+                    {this.props.slot}
+                </Button>
+
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        {this.props.slot}
+                    </Modal.Header>
+                    <Modal.Body>
+                        <ul> {rows} </ul>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: {
+                belt: 0,
+                boots: 0,
+            },
+        };
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Slot</th>
+                            <th>Item</th>
+                            <th>Hit %</th>
+                            <th>Crit %</th>
+                            <th>AP</th>
+                            <th>Agility</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><Picker slot="Belt" gear={Belts} /></td>
+                            <td>Giantstalker's Helmet</td>
+                            <td>0</td>
+                            <td>1</td>
+                            <td>0</td>
+                            <td>11</td>
+                        </tr>
+                        <tr>
+                            <td>Head Enchant</td>
+                            <td>Lesser Arcanum of Voracity</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>8</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
+
 }
 
 export default App;
